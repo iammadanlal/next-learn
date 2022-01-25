06664c1cb97b67ff8eb9057af2e85f96b17c8876
Madan Lal <iammadanlalmali@gmail.com>
2022-01-26 04:01:48 +0530

    Adding CSS & Modules

---
+++ pages/_app.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+// import "../src/styles.css";
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import React from "react";
+import "../src/styles.module.css";
 
 const Page = () => (
   <div>
