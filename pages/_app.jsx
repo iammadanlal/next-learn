7a1302e7e42f3fb03cc9d106979658d3db61332b
Madan Lal <iammadanlalmali@gmail.com>
2022-01-29 02:06:37 +0530

    Adding Theme UI

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 // import "../src/styles.css";
+import { ThemeProvider } from "theme-ui";
+import theme from "../theme";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
