8954c86316994d42a7c56ab4fe12aedaded2a9d6
Madan Lal <iammadanlalmali@gmail.com>
2022-01-29 20:27:44 +0530

    Plugins

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
-const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
-module.exports = (phase, { defaultConfig }) => {
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    console.log("i am in dev mode!");
-    return defaultConfig;
-  }
-  return defaultConfig;
-};
+const nextEnv = require("next-env");
+const dotenvLoad = require("dotenv-load");
+// const
+
+dotenvLoad();
+
+const withEnv = nextEnv();
+module.exports = withEnv();
