fdf1f5f5902080991f64397f27a110cd9b42e8b2
Madan Lal <iammadanlalmali@gmail.com>
2022-01-30 19:39:54 +0530

    Fetching notes with getServerSideProps

--- pages/notes/index.jsx
+++ pages/notes/index.jsx
@@ -1,11 +1,7 @@
 import { jsx } from "theme-ui";
 import Link from "next/link";
 
-export default () => {
-  const notes = new Array(15)
-    .fill(1)
-    .map((e, i) => ({ id: i, title: `This is my note ${i}` }));
-
+export default ({ notes }) => {
   return (
     <div sx={{ variant: "containers.page" }}>
       <h1>My Notes</h1>
@@ -33,3 +29,14 @@ export default () => {
     </div>
   );
 };
+
+export async function getServerSideProps() {
+  const res = await fetch("http://localhost:3000/api/note");
+  const { data: notes } = await res.json();
+
+  return {
+    props: {
+      notes,
+    },
+  };
+}
--- src/data/data.js
+++ src/data/data.js
@@ -1,3 +1,6 @@
-const notes = [];
+const notes = new Array(15).fill(1).map((_, i) => ({
+  id: Date.now() + i,
+  title: `Note ${i}`,
+}));
 
 module.exports = notes;
