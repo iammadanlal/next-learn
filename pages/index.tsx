862a0f3166edc48f5f157e84b809d694902e4a6e
Madan Lal <iammadanlalmali@gmail.com>
2022-01-30 20:19:32 +0530

    Working with SSR

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,10 @@
 import { jsx } from "theme-ui";
 import Link from "next/link";
+import dynamic from "next/dynamic";
+
+const BrowserComponent = dynamic(() => import("../src/components/browser"), {
+  ssr: false,
+});
 
 export default ({ content }) => (
   <div sx={{ height: `calc(100vh - 60px)` }}>
@@ -12,6 +17,7 @@ export default ({ content }) => (
       }}
     >
       <h1 sx={{ fontSize: 8, my: 0 }}>{content.title}</h1>
+      <BrowserComponent />
     </div>
   </div>
 );
---
+++ src/components/browser.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+console.log(document.body);
+
+export default () => <div>Browser Component</div>;
